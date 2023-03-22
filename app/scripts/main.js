da61473d2fffc5b1ca5d01eb7133e7c42c3e5eea
fe-lam-dinh <dinhtronglam172000@gmail.com>
2023-03-22 14:45:29 +0700

    FEAT: navigation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,25 @@
 window.addEventListener("load", function () {
-  const toggle = document.querySelector(".menu-toggle");
-  const menu = document.querySelector(".menu");
+  const toggle = document.querySelector(".header-bar");
+  const menu = document.querySelector(".navigation-primary");
+  const closeMenuMobile = this.document.querySelector(
+    ".navigation-primary__header-ic"
+  );
+
   toggle && toggle.addEventListener("click", handleToggleMenu);
   function handleToggleMenu(e) {
     menu && menu.classList.add("is-show");
   }
+  closeMenuMobile &&
+    closeMenuMobile.addEventListener("click", handleClickReMoveMenu);
+  function handleClickReMoveMenu(e) {
+    menu && menu.classList.remove("is-show");
+  }
   document.addEventListener("click", handleClickOutside);
   function handleClickOutside(e) {
-    if (e.target.matches(".menu-toggle") || e.target.matches(".menu, .menu *"))
+    if (
+      e.target.matches(".header-bar") ||
+      e.target.matches(".navigation-primary, .navigation-primary *")
+    )
       return;
     menu && menu.classList.remove("is-show");
   }
