cf2813dfabd15c396632127b7afb914cd58aea1c
fe-lam-dinh <dinhtronglam172000@gmail.com>
2023-04-04 14:25:50 +0700

    FEAT: navigation mobile

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,24 @@
 window.addEventListener("load", function () {
+  // toggle menu mobile
   const toggle = document.querySelector(".header-bar");
   const menu = document.querySelector(".navigation-primary");
-  const closeMenuMobile = this.document.querySelector(
+  const closeMenuMobile = document.querySelector(
     ".navigation-primary__header-ic"
   );
 
-  toggle && toggle.addEventListener("click", handleToggleMenu);
+  const navigationPrimaryItem = document.querySelectorAll(
+    ".navigation-primary__item"
+  );
+
+  // handle toggle menu
   function handleToggleMenu(e) {
     menu && menu.classList.add("is-show");
   }
-  closeMenuMobile &&
-    closeMenuMobile.addEventListener("click", handleClickReMoveMenu);
+  // handle click remove menu
   function handleClickReMoveMenu(e) {
     menu && menu.classList.remove("is-show");
   }
-  document.addEventListener("click", handleClickOutside);
+  // handle click outside
   function handleClickOutside(e) {
     if (
       e.target.matches(".header-bar") ||
@@ -23,4 +27,21 @@ window.addEventListener("load", function () {
       return;
     menu && menu.classList.remove("is-show");
   }
-});
+  // add event click toggle menu
+  toggle && toggle.addEventListener("click", handleToggleMenu);
+  // add event click remove menu
+  closeMenuMobile &&
+    closeMenuMobile.addEventListener("click", handleClickReMoveMenu);
+
+  // add event click handle click outside
+  document.addEventListener("click", handleClickOutside);
+
+  //Toggle navigation item
+  [...navigationPrimaryItem].forEach((item) => {
+    item.addEventListener("click", (e) => {
+      const subMenu = e.target.querySelector(".navigation-primary__sub");
+      subMenu && subMenu.classList.toggle("is-show");
+      console.log(e.target);
+    });
+  });
+}); // end load document
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,18 +1,37 @@
 window.addEventListener("load", function () {
+  // toggle menu mobile
   const toggle = document.querySelector(".header-bar");
   const menu = document.querySelector(".navigation-primary");
-  const closeMenuMobile = this.document.querySelector(".navigation-primary__header-ic");
-  toggle && toggle.addEventListener("click", handleToggleMenu);
+  const closeMenuMobile = document.querySelector(".navigation-primary__header-ic");
+  const navigationPrimaryItem = document.querySelectorAll(".navigation-primary__item");
+
+  // handle toggle menu
   function handleToggleMenu(e) {
     menu && menu.classList.add("is-show");
   }
-  closeMenuMobile && closeMenuMobile.addEventListener("click", handleClickReMoveMenu);
+  // handle click remove menu
   function handleClickReMoveMenu(e) {
     menu && menu.classList.remove("is-show");
   }
-  document.addEventListener("click", handleClickOutside);
+  // handle click outside
   function handleClickOutside(e) {
     if (e.target.matches(".header-bar") || e.target.matches(".navigation-primary, .navigation-primary *")) return;
     menu && menu.classList.remove("is-show");
   }
-});
+  // add event click toggle menu
+  toggle && toggle.addEventListener("click", handleToggleMenu);
+  // add event click remove menu
+  closeMenuMobile && closeMenuMobile.addEventListener("click", handleClickReMoveMenu);
+
+  // add event click handle click outside
+  document.addEventListener("click", handleClickOutside);
+
+  //Toggle navigation item
+  [...navigationPrimaryItem].forEach(item => {
+    item.addEventListener("click", e => {
+      const subMenu = e.target.querySelector(".navigation-primary__sub");
+      subMenu && subMenu.classList.toggle("is-show");
+      console.log(e.target);
+    });
+  });
+}); // end load document
